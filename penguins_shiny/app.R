a5a26604a64d11866c820640637dc06c518b585e
katelinseeto <katelinseeto@gmail.com>
2021-01-19 16:19:34 -0800

    Made changes to penguins shiny app from teaching key on gauchospace

--- penguins_shiny/app.R
+++ penguins_shiny/app.R
@@ -10,10 +10,17 @@ ui <- fluidPage(
         sidebarPanel("put my widgets here!",
                      radioButtons(inputId = "penguin_species", #how widget is stored
                                   label = "Choose penguin species:", #how is labeled
-                                  choices = c("Adelie", "Cool Chinstrap" = "Chinstrap",                                               "Gentoo"))
+                                  choices = c("Adelie", "Cool Chinstrap" = "Chinstrap",                                               "Gentoo"),
+                     ),
+                                  selectInput(inputId = "pt_color",
+                                              label = "Select point color",
+                                              choices = c("Awesome red!" = "red",
+                                                          "Pretty purple" = "purple",
+                                                          "ORAAANGE" = "orange"))
                      ),
         mainPanel("Here's my graph!",
-                  plotOutput(outputId = "penguin_plot"))
+                  plotOutput(outputId = "penguin_plot"),
+                  tableOutput(outputId = "penguin_table"))
     )
 )
 
@@ -25,9 +32,24 @@ server <- function(input, output)  {
             filter(species == input$penguin_species)
     })
 
+    penguin_table <- reactive({
+        penguins %>%
+            filter(species == input$penguin_species) %>%
+            drop_na() %>%
+            group_by(sex) %>%
+            summarise(
+                mean_flip = mean(flipper_length_mm),
+                mean_mass = mean(body_mass_g)
+            )
+    })
+
     output$penguin_plot <- renderPlot({
         ggplot(data = penguin_select(), aes(x = flipper_length_mm, y = body_mass_g))+
-            geom_point()
+            geom_point(color = input$pt_color)
+    })
+
+    output$penguin_table <- renderTable({
+        penguin_table()
     })
 
 }
